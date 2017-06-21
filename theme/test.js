5433e9d474f0d332003e61777b4ee1ccbc0fb713
Евгений Плотников <bad4iz@gmail.com>
2017-06-21 07:43:30 +0400

    в

--- theme/test.js
+++ theme/test.js
@@ -1,20 +1,20 @@
-// function sssd() {
+function sssd() {
     
-// var cards = document.querySelectorAll('.list-card');
+var cards = document.querySelectorAll('.list-card');
 
-// cards.forEach(card=> {
-//     var tmp = card.querySelector('.icon-checklist');
-// var proc = 0;
-// if (tmp) {
-//     if (tmp.nextSibling) {
-//         badge = tmp.nextSibling.textContent.split("/")
-//         proc = badge[0] ? badge[0]/badge[1]*100 : 0;
-//     }
-//         console.log(proc);
-// }
-// card.innerHTML += '<div class="scrum" style="background-color:rgb(75, 191, 107);width: ' + proc + '%;position: absolute;top: 0px;left: 0px;height: 100%;opacity: 0.2;z-index: -1;background: rgb(55, 158, 90);"></div>'
-// });
-// }
+cards.forEach(card=> {
+    var tmp = card.querySelector('.icon-checklist');
+var proc = 0;
+if (tmp) {
+    if (tmp.nextSibling) {
+        badge = tmp.nextSibling.textContent.split("/")
+        proc = badge[0] ? badge[0]/badge[1]*100 : 0;
+    }
+        console.log(proc);
+}
+card.innerHTML += '<div class="scrum" style="background-color:rgb(75, 191, 107);width: ' + proc + '%;position: absolute;top: 0px;left: 0px;height: 100%;opacity: 0.2;z-index: -1;background: rgb(55, 158, 90);"></div>'
+});
+}
 
 // setInterval(sssd, 10000);
 
@@ -29,3 +29,10 @@ console.log('включился 3');
 // chrome.bookmarks.create({'parentId': extensionsFolderId,
 //                                'title': 'Extensions doc',
 //                                'url': 'http://code.google.com/chrome/extensions'});
+
+//
+// badges.forEach(badge=>{
+//     badge.style.border='1px solid #dcdcdc';
+// badge.style.boxShadow='1px 1px 1px';
+// }
+// );
